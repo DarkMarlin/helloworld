798bdfa54195ec26c48024c81c7703279637ed12
Tyler Matijasich <tylermatijasich@gmail.com>
2023-01-23 16:20:36 -0500

    Added helloworld

--- src/index.js
+++ src/index.js
@@ -4,12 +4,9 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+var element = React.createElement('h1', { className: 'greeting' }, 'Hello, world!');
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+root.render(element);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
